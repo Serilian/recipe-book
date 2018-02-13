e5d7fdf3f86f161dd96c5d191a02593c5ff40179
Filip <fhagno@yahoo.com>
2018-02-13 18:19:31 +0100

    Some code added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {selector} from "rxjs/operator/publish";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+
+})
+
+export class HeaderComponent {
+
+}
