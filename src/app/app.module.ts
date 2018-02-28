97c0ab496f778b56468234b5799d5445cb152fab
Filip <fhagno@yahoo.com>
2018-02-28 18:11:47 +0100

    Some code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {DropdownDirective} from "./shared/dropdown.directive";
 
 
 @NgModule({
@@ -21,10 +24,13 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,13 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {selector} from "rxjs/operator/publish";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
-
 })
-
 export class HeaderComponent {
-
   @Output() featureSelected = new EventEmitter<string>();
 
   onSelect(feature: string) {
-
     this.featureSelected.emit(feature);
-
   }
-
 }
