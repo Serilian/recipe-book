8408cc938183b8875623a2f922111f261418fde7
Filip <fhagno@yahoo.com>
2018-04-14 08:40:47 +0200

    Another episode added.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
 
-  constructor(private recipesService: RecipeService, private route: ActivatedRoute, private router: Router) { }
+  subscription: Subscription;
+
+  constructor(private recipesService: RecipeService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit() {
-    this.recipesService.recipesChanged.subscribe(
-    (recipes: Recipe[]) => {
-      this.recipes = recipes;
-    });
+   this.subscription = this.recipesService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      });
     this.recipes = this.recipesService.getRecipes();
   }
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
