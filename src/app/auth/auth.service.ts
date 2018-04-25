30d0eec85336913a1d5865f1fef3ce8a3fed0aba
Filip <fhagno@yahoo.com>
2018-04-25 14:22:36 +0200

    Route protection

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import * as firebase from 'firebase';
+import {Injectable} from "@angular/core";
+import {Router} from "@angular/router";
+import {routerNgProbeToken} from "@angular/router/src/router_module";
 
+@Injectable()
 export class AuthService {
 
   token: string;
+
+
+  constructor(private router: Router) {}
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -19,6 +26,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         (response) => {
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getToken()
             .then(
               (token: string) => this.token = token
