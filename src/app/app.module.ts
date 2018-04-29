9435255ce0ecedef04802f798422e4b98debd3b6
Filip <fhagno@yahoo.com>
2018-04-29 10:27:09 +0200

    Addding core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
+import {HeaderComponent} from './core/header/header.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from "./recipes/recipe.service";
@@ -14,14 +12,13 @@ import {AuthGuardService} from "./auth/auth-guard.service";
 import {SharedModule} from "./shared/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AuthModule} from "./auth/auth.module";
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
+import {CoreModule} from "./core/core.module";
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +26,8 @@ import { HomeComponent } from './home/home.component';
     HttpModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule
   ],
   bootstrap: [AppComponent],
   providers: [
