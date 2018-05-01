c4e9e7e53bff0f23efea7e37910db530de0a0126
Filip <fhagno@yahoo.com>
2018-05-01 10:37:10 +0200

    Bug fixes and optimalizations

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
+import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
 import {RecipesComponent} from "./recipes.component";
 import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
 import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
