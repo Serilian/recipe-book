a9ba6eefd52a997a59731126a6b74b821167a404
Filip <fhagno@yahoo.com>
2018-05-05 07:32:24 +0200

    Changing to Http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './core/header/header.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {RecipeService} from "./recipes/recipe.service";
-import {HttpModule} from "@angular/http";
-import {DataStorageService} from "./shared/data-storage.service";
-import {AuthService} from "./auth/auth.service";
-import {AuthGuardService} from "./auth/auth-guard.service";
 import {SharedModule} from "./shared/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AuthModule} from "./auth/auth.module";
-import { HomeComponent } from './core/home/home.component';
 import {CoreModule} from "./core/core.module";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -23,11 +16,11 @@ import {CoreModule} from "./core/core.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule,
     SharedModule,
     ShoppingListModule,
     AuthModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent],
   providers: []
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,14 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/add/operator/map';
 import {AuthService} from "../auth/auth.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService
   ) {
@@ -16,15 +16,14 @@ export class DataStorageService {
 
   storeData() {
     const token = this.authService.getToken();
-    return this.http.put('https://ng-recipe-book-9f137.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.httpClient.put('https://ng-recipe-book-9f137.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   fetchData() {
     const token = this.authService.getToken();
-    this.http.get('https://ng-recipe-book-9f137.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-9f137.firebaseio.com/recipes.json?auth=' + token)
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
